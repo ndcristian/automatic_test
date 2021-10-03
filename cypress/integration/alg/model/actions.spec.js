e5febf24c0d8c8bef9e06498697db1512c1c5d5d
cris <ndcristian@gmail.com>
2021-10-03 13:01:45 +0300

    add ui-demo test

--- cypress/integration/alg/model/actions.spec.js
+++ cypress/integration/alg/model/actions.spec.js
@@ -5,11 +5,17 @@ describe('test model', ()=>{
 
   it('correct menu', ()=>{
 
-    cy.visit('http://localhost:4200/')
-    cy.contains('Alumgates v2')
-    cy.contains('Ofertare')
-    cy.contains('Admin')
-    cy.contains('Login')
+ cy.visit('https://ui-demo.zf-v2x.com/')
+    cy.contains('Welcome')
+    cy.contains('Next')
+    cy.contains('ZF AD')
+    
+
+    // cy.visit('http://localhost:4200/')
+    // cy.contains('Alumgates v2')
+    // cy.contains('Ofertare')
+    // cy.contains('Admin')
+    // cy.contains('Login')
     // cy.contains('Logout')
 
   })
